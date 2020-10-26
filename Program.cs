17aa5cb646c16ea689aa66f3d3ea5b14ba1a1af2
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-26 11:09:00 +0530

    [tejas] add Initial Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree");
+            Tree<int> binarySearch = new Tree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
+            binarySearch.Display();
+            binarySearch.GetSize();
+
+            bool result = binarySearch.ifExists(67, binarySearch);
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class Tree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public Tree<T> LeftTree { get; set; }
+        public Tree<T> RightTree { get; set; }
+        public Tree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0;
+        int rightCount = 0;
+        bool result = false;
+
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new Tree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+
+
+            }
+            else if (this.RightTree == null)
+                this.RightTree = new Tree<T>(item);
+            else
+                this.RightTree.Insert(item);
+
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+
+            }
+
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size " + this.leftCount + " " +this.rightCount );
+
+        }
+        public bool ifExists(T element, Tree<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found data in Tree: " + node.NodeData);
+                result = true;
+            }
+            else
+            {
+                Console.WriteLine("Current element {0} is not in tree", node.NodeData );
+
+            }
+            if (element.CompareTo(node.NodeData) < 0)
+            {
+                ifExists(element, node.LeftTree);
+            }
+            if (element.CompareTo(node.NodeData) > 0)
+            {
+                ifExists(element, node.RightTree);
+            }
+            return result;
+        }
+    }
+
+
+    }
+
